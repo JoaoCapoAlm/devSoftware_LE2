f3c9ca60c7659de2e68e72e106846865b4646fe6
João Pedro <jpcapoani98@gmail.com>
2024-03-18 00:08:26 -0300

    Projeto inicial

---
+++ src/Helper/Prompt.java
@@ -0,0 +1,162 @@
+package Helper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Um classe utilitária para imprimir e ler textos, números inteiros e decimais
+ * no prompt de comandos.
+ *
+ * @version 1.0 Mar 2023
+ * @author Geucimar Briatore
+ */
+public class Prompt {
+    /**
+     * Imprime a mensagem enviada no console.
+     */
+    public static void imprimir(String mensagem, boolean newLine) {
+        if(newLine)
+            System.out.println(mensagem);
+        else
+            System.out.print(mensagem);
+        System.out.flush();
+    }
+
+    /**
+     * Imprime o objeto enviado no console.
+     */
+    public static void imprimir(Object object) {
+        System.out.println(object);
+        System.out.flush();
+    }
+
+    public static void imprimirArray(int[] array, boolean newLine){
+        if(array == null){
+            return;
+        }
+        for(var i : array){
+            imprimir(String.valueOf(i), newLine);
+        }
+    }
+
+    public static void imprimirErro(String mensagem, boolean newLine){
+        if(newLine)
+            System.err.println(mensagem);
+        else
+            System.err.print(mensagem);
+        System.err.flush();
+    }
+
+    /**
+     * Imprime uma linha separadora no console.
+     */
+    public static void separador() {
+        imprimir("---------------------------------------------------", true);
+    }
+
+    /**
+     * Imprime uma linha em branco no console.
+     */
+    public static void linhaEmBranco() {
+        imprimir("", true);
+    }
+
+    /**
+     * Imprime a mensagem enviada, faz a leitura do texto digitado no prompt de
+     * comandos e retorna uma String no formato ISO-8859-1 Latin ou UTF-8.
+     *
+     * @return String
+     */
+    public static String lerLinha(String mensagem) {
+        imprimir(mensagem);
+        return lerLinha();
+    }
+
+    /**
+     * Faz a leitura de texto digitado no prompt de comandos e retorna uma String no
+     * formato ISO-8859-1 Latin ou UTF-8.
+     *
+     * @return String
+     */
+    public static String lerLinha() {
+        while (true) {
+            try {
+                InputStreamReader isr = new InputStreamReader(System.in);
+                BufferedReader br = new BufferedReader(isr);
+                return br.readLine();
+            } catch (IOException e) {
+                imprimirErro("Texto inválido, digite novamente...", true);
+            }
+        }
+    }
+
+    /**
+     * Imprime a mensagem enviada, faz a leitura do número digitado no prompt de
+     * comandos e retorna um int.
+     *
+     * @return int
+     */
+    public static int lerInteiro(String mensagem, boolean newLine) {
+        imprimir(mensagem, newLine);
+        return lerInteiro();
+    }
+
+    /**
+     * Faz a leitura do número digitado no prompt de comandos e retorna um int.
+     *
+     * @return int
+     */
+    private static int lerInteiro() {
+        while (true) {
+            try {
+                String linha = lerLinha();
+                if (linha.isEmpty()) {
+                    return 0;
+                }
+                return Integer.parseInt(linha);
+            } catch (NumberFormatException tExcept) {
+                imprimirErro("Inteiro inválido, digite novamente...", true);
+            }
+        }
+    }
+
+    /**
+     * Imprime a mensagem enviada, faz a leitura do número digitado no prompt de
+     * comandos e retorna um double.
+     *
+     * @return double
+     */
+    public static double lerDecimal(String mensagem, boolean newLine) {
+        imprimir(mensagem, newLine);
+        return lerDecimal();
+    }
+
+    /**
+     * Faz a leitura do número digitado no prompt de comandos e retorna um double.
+     *
+     * @return double
+     */
+    private static double lerDecimal() {
+        while (true) {
+            try {
+                String linha = lerLinha();
+                if (linha.isEmpty()) {
+                    return 0;
+                }
+                return Double.parseDouble(linha);
+            } catch (NumberFormatException e) {
+                imprimirErro("Decimal inválido, digite novamente...", true);
+            }
+        }
+    }
+
+    /**
+     * Faz uma pausa no processamento e pede para o usuário pressionar enter para
+     * continuar.
+     */
+    public static void pressionarEnter() {
+        imprimir("Pressione ENTER para continuar...", false);
+        lerLinha();
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,56 @@
+import Helper.Prompt;
+
+public class Main {
+    public static void main(String[] args) {
+        MostrarMenu();
+
+        var executar = true;
+        while (executar) {
+            switch (Prompt.lerInteiro("Escolha uma opçao: ", false)) {
+                case 1 -> Prompt.imprimir("Exercício 01");
+                default -> Prompt.imprimirErro("Opção inválida!", true);
+            }
+
+            Prompt.separador();
+            Prompt.linhaEmBranco();
+
+            var voltarMenu = true;
+            while (voltarMenu) {
+                var optVoltarMenu = Prompt.lerLinha("Deseja voltar ao menu? (S)im ou (N)ão?");
+                if (optVoltarMenu.equalsIgnoreCase("S")) {
+                    voltarMenu = false;
+                    MostrarMenu();
+                } else if (optVoltarMenu.equalsIgnoreCase("N")) {
+                    executar = false;
+                    voltarMenu = false;
+                } else {
+                    Prompt.imprimirErro("Opção inválida!", true);
+                }
+            }
+        }
+
+        Prompt.pressionarEnter();
+    }
+
+    public static void MostrarMenu() {
+        Prompt.separador();
+        Prompt.imprimir("Menu", true);
+        Prompt.imprimir("1 - Exercício 01 -     ", true);
+        Prompt.imprimir("2 - Exercício 02 - ", true);
+        Prompt.imprimir("3 - Exercício 03 - ", true);
+        Prompt.imprimir("4 - Exercício 04 - ", true);
+        Prompt.imprimir("5 - Exercício 05 - ", true);
+        Prompt.imprimir("6 - Exercício 06 - ", true);
+        Prompt.imprimir("7 - Exercício 07 - ", true);
+        Prompt.imprimir("8 - Exercício 08 - ", true);
+        Prompt.imprimir("9 - Exercício 09 - ", true);
+        Prompt.imprimir("10 - Exercício 10 - ", true);
+        Prompt.imprimir("11 - Exercício 11 - ", true);
+        Prompt.imprimir("12 - Exercício 12 - ", true);
+        Prompt.imprimir("13 - Exercício 13 - ", true);
+        Prompt.imprimir("14 - Exercício 14 - ", true);
+        Prompt.imprimir("15 - Exercício 15 - ", true);
+        Prompt.separador();
+        Prompt.linhaEmBranco();
+    }
+}
